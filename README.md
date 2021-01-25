ddcd00928ab3f8839315d366fa14502d864c3d34
Wang Hao <goinfrerie@gmail.com>
2021-01-25 13:08:59 +0800

    clean logic for generating random grids

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # mini-projects
+
+to generate randomized 2D matrix, for pathfinding
+design:
+params:
+m x n rectangle shaped matrix
+start point, end point
+intensity level of obstacles
+guaranteed path/paths
+
+obstacles represented by 1 no obstacle is 0
+
+functions required:
+    f -> generate guaranteed path
+    f -> generate grids
+    f -> apply generated guaranteed path onto grids (sweep out)
--- src/generate_grid.py
+++ src/generate_grid.py
@@ -1,43 +1,52 @@
-# to generate randomized 2D matrix, for pathfinding
-import numpy
-from typing import *
-
-# design:
-# params:
-# m x n rectangle shaped matrix
-# start point, end point
-# intensity level of obstacles
-# guaranteed path/paths
-
-# obstacles represented by 1
-# no obstacle is 0
-
-# functions required:
-# f -> generate guaranteed path
-# f -> generate grids
-# f -> apply generated guaranteed path onto grids (sweep out)
-
 import numpy as np
 import argparse
 
 
 def gen_randomized_grids(m: int, n: int, block_intensity: float) -> np.ndarray:
+    """to generate randomized 2d matrix
+
+    :param m: rectangle x axis
+    :param n: rectangle y axis
+    :param block_intensity: binomial distribution param to generate blocks range 0 to 1
+    :return: 2d array, 1 is obstacle
+    """
     return np.random.binomial(n=1, p=block_intensity, size=[m, n])
 
 
-def gen_guaranteed_path(start: tuple, end: tuple) -> None:
-    return None
+def gen_guaranteed_path(start: str, end: str, grids: np.ndarray) -> np.ndarray:
+    """generate a fixed guaranteed cross-shape path from start to end
+
+    :param start: comma separated x,y
+    :param end: comma separated x,y
+    :param grids: output from gen_randomized_grids
+    :return: modified grids, with garanteed path from start to end
+    """
+    unblock_x = int(start.split(",")[0])
+    unblock_y = int(end.split(",")[1])
+    try:
+        grids[:, unblock_y] = 0
+        grids[unblock_x, :] = 0
+    except:
+        print("size mismatch")
+    return grids
 
 
-def main(m: int, n: int, block_intensity: float) -> None:
-    print(gen_randomized_grids(m, n, block_intensity))
+def main(m: int, n: int, block_intensity: float, guarantee: str, start: str, end: str) -> None:
+    grids = gen_randomized_grids(m, n, block_intensity)
+    if guarantee == "True":
+        grids = gen_guaranteed_path(start, end, grids)
+    print(grids)
+    return grids
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='create basic grid')
-    parser.add_argument('--m', type=str, required=True, help='where the model is saved')
-    parser.add_argument('--n', type=str, required=True, help='path to the prediction data')
-    parser.add_argument('--intensity', type=str, required=True, help='path to the prediction data')
+    parser.add_argument('--m', type=str, required=True, help='x axis length')
+    parser.add_argument('--n', type=str, required=True, help='y axis length')
+    parser.add_argument('--intensity', type=str, required=True, help='binomial dist for obstacle intensity')
+    parser.add_argument('--guarantee', type=str, required=True, help='if garanteed a path exist from start to end')
+    parser.add_argument('--start', type=str, required=False, help='given start point')
+    parser.add_argument('--end', type=str, required=False, help='given end point')
 
     args = parser.parse_args()
-    main(int(args.m), int(args.n), float(args.intensity))
+    main(int(args.m), int(args.n), float(args.intensity), args.guarantee, args.start, args.end)
